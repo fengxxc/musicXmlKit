ef1158d1ba0e82fd13959e3541db99683a7b5bc1
fengxxc <744320491@qq.com>
2020-06-04 11:49:46 +0800

    测试框架改为Jest

--- test/model/node.test.ts
+++ test/model/node.test.ts
@@ -1,7 +1,6 @@
 import { Node } from "../../src/model/node";
-import { expect } from "chai";
 
-describe('Node test', () => {
+test('Node test', () => {
     const lijing = new Node(0, null, '李靖', {'外号': '托塔天王', '技能': '百分百空手接白刃', '兵器': '玲珑宝塔'});
     lijing.setText('孽障，休得无礼！');
     const jinzha = new Node(0, lijing, '金吒', {'外号': '大太子', '技能': '找师傅', '兵器': '吴钩剑'});
@@ -11,29 +10,25 @@ describe('Node test', () => {
     const nezha2 = new Node(2, lijing, '哪吒', {'外号': '三太子', '技能': ['三昧真火', '藕断丝连'], '兵器': ['火尖枪', '乾坤圈', '混天绫', '风火轮', '幡天章']});
     nezha.setText('师傅讨厌啦~');
 
-    it('expect: node.getName', () => expect(nezha.getName()).to.be.equal('哪吒'));
+    expect(nezha.getName()).toBe('哪吒');
 
-    it('expect: node.getIndex', () => expect(nezha2.getIndex()).to.be.equal(2));
+    expect(nezha2.getIndex()).toBe(2);
 
-    it('expect: node.getAttr', () => 
-        expect(nezha2.getAttr()).to.be.deep.equal({'外号': '三太子', '技能': ['三昧真火', '藕断丝连'], '兵器': ['火尖枪', '乾坤圈', '混天绫', '风火轮', '幡天章']})
-    );
+   expect(nezha2.getAttr()).toEqual({'外号': '三太子', '技能': ['三昧真火', '藕断丝连'], '兵器': ['火尖枪', '乾坤圈', '混天绫', '风火轮', '幡天章']});
 
-    it('expect: node.getText', () => expect(nezha.getText()).to.be.equal('师傅讨厌啦~'));
+    expect(nezha.getText()).toBe('师傅讨厌啦~');
  
-    it('expect: node.getFullText', () => expect(nezha.getFullText()).to.be.equal('师傅讨厌啦~'));
+    expect(nezha.getFullText()).toBe('师傅讨厌啦~');
 
-    it('expect: node.getParentNode', () => expect(nezha.getParentNode().getName()).to.be.equal('李靖'));
+    expect(nezha.getParentNode().getName()).toBe('李靖');
 
     // console.log(lijing.getChildNodesByName('哪吒'))
     // console.log(lijing._childAttrIndex)
-    it('expect: node.getChildNodesByName', () => {
-        expect(lijing.getChildNodesByName('金吒')[0]).to.be.equal(jinzha);
-        expect(lijing.getChildNodesByName('哪吒')[0]).to.be.equal(nezha);
-        expect(lijing.getChildNodesByName('哪吒')[1]).to.be.equal(nezha2);
-    });
+    expect(lijing.getChildNodesByName('金吒')[0]).toEqual(jinzha);
+    expect(lijing.getChildNodesByName('哪吒')[0]).toEqual(nezha);
+    expect(lijing.getChildNodesByName('哪吒')[1]).toEqual(nezha2);
 
-    it('expect: node.getChildNodesByName deep', () => expect(lijing.getChildNodesByName('哪吒')[0]).to.be.deep.equal(nezha));
+    expect(lijing.getChildNodesByName('哪吒')[0]).toEqual(nezha);
 
-    it('expect: node.getChildSize', () => expect(lijing.getChildSize()).to.be.deep.equal(3));
+    expect(lijing.getChildSize()).toEqual(3);
 })
--- test/model/parser.test.ts
+++ test/model/parser.test.ts
@@ -1,18 +1,16 @@
 import { Parser } from "../../src/model/parser";
 import { SorePartWiseNode } from "../../src/model/sorePartWiseNode";
-import { expect } from "chai";
 import { readFileSync } from "fs";
 import { MeasureNode } from "../../src/model/measureNode";
 
-describe('Parser test', () => {
+test('Parser test', () => {
     const xml = readFileSync('./test/model/testData1.xml', 'utf-8');
     const root = Parser.parseMusicXml(xml);
     const spn = <SorePartWiseNode>root.getChildNodesByName('score-partwise')[0];
 
-    it('expect: score-partwise and measure', () => {
-        expect(spn.getName()).to.be.equal('score-partwise');
-        const m: MeasureNode[] = <MeasureNode[]>spn.getChildNodesByName('part')[0].getChildNodesByName('measure');
-        expect(m[0].Number()).to.be.equal(1);
-    });
+    expect(spn.getName()).toBe('score-partwise');
+
+    const m: MeasureNode[] = <MeasureNode[]>spn.getChildNodesByName('part')[0].getChildNodesByName('measure');
+    expect(m[0].Number()).toBe(1);
 
 })
