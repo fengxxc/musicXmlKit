de8b761bcf532061e724496d9dd8bf18e3183713
fengxxc <744320491@qq.com>
2020-08-09 20:48:16 +0800

    修复token没有继承上一小节属性的bug

--- src/view/mxIterator.ts
+++ src/view/mxIterator.ts
@@ -9,24 +9,31 @@ export class MxIterator {
     static *getIterator(musicXmlNode: RootNode) {
         const spw: SorePartWiseNode = <SorePartWiseNode> musicXmlNode.getChildNodesByName('score-partwise')[0];
         const parts: Node[] = spw.Part();
+        let attr: any = {};
         for (const part of parts) {
             for (const measure of part.getChildNodes()) {
                 const _measure: MeasureNode = <MeasureNode>measure;
                 const attributes: AttributesNode = _measure.Attributes();
+                attr['keyFifths'] = attributes.KeyFifths() ? attributes.KeyFifths() : attr['keyFifths'];
+                attr['keyMode'] = attributes.KeyMode() ? attributes.KeyMode() : attr['keyMode'];
+                attr['timeBeatType'] = attributes.TimeBeatType() ? attributes.TimeBeatType() : attr['timeBeatType'];
+                attr['timeBeats'] = attributes.TimeBeats() ? attributes.TimeBeats() : attr['timeBeats'];
+                attr['clef'] = (attributes.Clef() != null && attributes.Clef().length > 0) ? 
+                                    attributes.Clef().map(c => new ClefToken(c.Number(), c.Sign(), c.Line())) : attr['clef'];
                 for (const spirit of _measure.displayEntities()) {
                     yield new MxToken(
                         // 第几小节
                         _measure.Number(),
                         // 五度圈里的位置，暂时这么理解
-                        attributes.KeyFifths(),
+                        attr['keyFifths'],
                         // 调性：大调、小调 [major | minor]
-                        attributes.KeyMode(),
+                        attr['keyMode'],
                         // 以几分音符为一拍
-                        attributes.TimeBeatType(),
+                        attr['timeBeatType'],
                         // 每小节有几拍
-                        attributes.TimeBeats(),
+                        attr['timeBeats'],
                         // 谱号 [ {number: 1, sign: 'G', line: 2}, {number: 2, sign: TAB, line: 5} , ...]
-                        attributes.Clef().map(c => new ClefToken(c.Number(), c.Sign(), c.Line())),
+                        attr['clef'],
                         // 要渲染的这个小东西，是什么类型，如: direction | note | backup | ...
                         spirit.getName(),
                         spirit
@@ -36,4 +43,5 @@ export class MxIterator {
         }
         return null;
     }
+
 }
