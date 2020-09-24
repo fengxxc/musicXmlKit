af49f9f1002a6b380506c8fb09c14cf9093aa466
fengxxc <fengxxc@gmail.com>
2020-09-24 12:32:29 +0800

    fix: 修复降调符号起始坐标错误

--- test/view/render_all_clef.test.ts
+++ test/view/render_all_clef.test.ts
@@ -5,7 +5,9 @@ import { Utils } from "../../src/utils";
 
 function main() {
     const xml: string = Utils.ajaxGetSync('/test/view/notes3.xml')
-    document.getElementById('can').insertAdjacentHTML('afterend', `<div style="display: inline-block; vertical-align: top; height: 800px; overflow: scroll;" > <xmp id="xml" style = "width: 850px;" >${xml}</xmp></div >`);
+    const can: HTMLElement = document.getElementById('can');
+    can.setAttribute('height', '2000');
+    can.insertAdjacentHTML('afterend', `<div style="display: inline-block; vertical-align: top; height: 2000px; overflow: scroll;" > <xmp id="xml" style = "width: 850px;" >${xml}</xmp></div >`);
     const root: RootNode = Parser.parseMusicXml(xml);
     const ctx: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById('can')
     Render.action(ctx, root);
